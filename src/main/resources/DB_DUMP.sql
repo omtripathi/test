fae187b5912fc2b4d41e69a5760a602d856e26f9
omtripathi <tripathiom15@gmail.com>
2017-09-05 16:28:08 +0530

    update db dump

--- src/main/resources/DB_DUMP.sql
+++ src/main/resources/DB_DUMP.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 03, 2017 at 08:26 PM
+-- Generation Time: Sep 05, 2017 at 10:56 AM
 -- Server version: 5.1.36
 -- PHP Version: 5.3.0
 
@@ -16,18 +16,41 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `property`
+--
+
+CREATE TABLE IF NOT EXISTS `property` (
+  `id` bigint(20) NOT NULL,
+  `State` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `place` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `property`
+--
+
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `ID` bigint(20) NOT NULL,
-  `FIRST_NAME` int(11) NOT NULL,
-  `LAST_NAME` int(11) NOT NULL,
-  `EMAIL` int(11) NOT NULL,
-  `MOBILE_NO` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `FULL_NAME` varchar(50) NOT NULL,
+  `USER_NAME` varchar(50) NOT NULL,
+  `PASSWORD` varchar(25) NOT NULL,
+  `MOBILE_NO` bigint(10) NOT NULL,
+  `EMAIL_ID` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=64 ;
 
 --
 -- Dumping data for table `user`
 --
+
 
